d0ad132cd486d58f33520c1d3fd8c9db4bf0f1e4
Maxim Yefremov <maxYefr@gmail.com>
2018-05-12 15:32:06 +0300

    parse xml response to js object

--- schema.js
+++ schema.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch')
+const util = require('util')
+const parseXML = util.promisify(require('xml2js').parseString)
 // api key can be generated here https://www.goodreads.com/api/keys
 const x = fetch(
     'https://www.goodreads.com/author/show.xml?id=4432&key=NsLXXc9qa7gXA5m5N83PYA'
 )
-    .then(response => response.text());
+    .then(response => response.text())
+    .then(parseXML)
 x
